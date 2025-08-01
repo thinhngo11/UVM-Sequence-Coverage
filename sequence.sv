0d4fa5f8d0423800e8d8c3d9a5b35add52594b29
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2025-08-01 14:42:15 +0700

    Update sequence.sv
    Update to run only the random sequence.

--- sequence.sv
+++ sequence.sv
@@ -11,14 +11,10 @@ class seq extends uvm_sequence #(seq_item);
   task body();
     `uvm_info("Random","Sequence",UVM_LOW);
     `uvm_do_with(req,{slv_addr==7'b0000000; rd_wr==0;})
-    repeat(1000) begin
-      `uvm_info("Random","Sequence",UVM_LOW);
+    repeat(2000) begin
+      //`uvm_info("Random","Sequence",UVM_LOW);
       `uvm_do_with(req,{slv_addr inside {4,8}; rpt==0;})
-/*      `uvm_do_with(req,{slv_addr inside {4,8}; rd_wr==0; rpt==0;})
-      i=req.int_addr;
-      a=req.slv_addr;
-      `uvm_do_with(req,{slv_addr==a; rd_wr==1; int_addr==i; rpt==0;})
-*/    end
+    end
   endtask
   
 endclass
