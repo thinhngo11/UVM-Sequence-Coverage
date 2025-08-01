b9fa6963de456d27e524181c4be9f28f48da9a02
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2025-08-01 14:35:06 +0700

    Update testbench.sv
    Updated to have two interfaces for multiple interface covercrosses.

--- testbench.sv
+++ testbench.sv
@@ -4,19 +4,30 @@ import uvm_pkg::*;
 `include "Slave_7b.sv"
 `include "Slave_10b.sv"
 `include "interface.sv"
+`include "interface1.sv"
 `include "tb_pkg.sv"
 
 module top;
   
-  intface intf();
+  intface intf0();
+  intface1 intf1();
   
-  Slave_7b#(4) sl1(intf.sda, intf.scl);
-  Slave_10b#(8) sl2(intf.sda, intf.scl);
-  assign intf.sda=1;
-  assign intf.scl=1;
+  Slave_7b#(4) sl1(intf0.sda, intf0.scl);
+  Slave_10b#(8) sl2(intf0.sda, intf0.scl);
+  assign intf0.sda=1;
+  assign intf0.scl=1;
   initial begin
-    uvm_config_db#(virtual intface)::set(uvm_root::get(),"*","driver",intf);
-    uvm_config_db#(virtual intface)::set(uvm_root::get(),"*","monitor",intf);
+    uvm_config_db#(virtual intface)::set(uvm_root::get(),"*","driver0",intf0);
+    uvm_config_db#(virtual intface)::set(uvm_root::get(),"*","monitor0",intf0);
+  end
+  
+  Slave_7b#(4) sl3(intf1.sda, intf1.scl);
+  Slave_10b#(8) sl4(intf1.sda, intf1.scl);
+  assign intf1.sda=1;
+  assign intf1.scl=1;
+  initial begin
+    uvm_config_db#(virtual intface1)::set(uvm_root::get(),"*","driver1",intf1);
+    uvm_config_db#(virtual intface1)::set(uvm_root::get(),"*","monitor1",intf1);
   end
   
   initial run_test("test");
@@ -29,12 +40,12 @@ module top;
   
   // To check clock streching
   initial begin
-    #62 @(negedge intf.scl);
-    force intf.scl=0;
-    #11 release intf.scl;
-    #185 @(negedge intf.scl);
-    force intf.scl=0;
-    #16 release intf.scl;
+    #62 @(negedge intf0.scl);
+    force intf0.scl=0;
+    #11 release intf0.scl;
+    #185 @(negedge intf0.scl);
+    force intf0.scl=0;
+    #16 release intf0.scl;
   end
   
 endmodule
