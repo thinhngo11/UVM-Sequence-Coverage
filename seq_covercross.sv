6a4b5685e56439cb903e2bdae9e04359c11fb6ad
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2025-08-01 14:44:50 +0700

    Update seq_covercross.sv

--- seq_covercross.sv
+++ seq_covercross.sv
@@ -1,24 +1,19 @@
-class seq_covercross extends uvm_sequence #(seq_item);
+class seq_covercross extends uvm_scoreboard;
   `uvm_object_utils(seq_covercross)
 
-  seq_item cseq1[$];
-  seq_item cseq2[$];
+  seq_item cseq1[$]; //to store coverage sequence1
+  seq_item cseq2[$]; //to store coverage sequence2
   seq_item q[$]; //to store sampled transactions
-  //logic cseq_covered[]; // set covered
-  int hit; // number of hits of seq coverage 
-  int index1; // the current index needs to be covered in the seq
-  int index2; // the current index needs to be covered in the seq
-  logic lstart, sstart, lend, send;
+  int hit; //number of hits of sequence cross coverage 
   string st;
   
-  function new(string name="seq_covercross");
-    super.new(name);
+  function new(string name="seq_covercross", uvm_component parent=null);
+    super.new(name,parent);
   	hit = 0;
-    //st = define it during new with specific comments like seq pattern
     st = name; 
   endfunction
   
-  // add items to queue to define coverage sequence pattern
+  //add items to queue to define coverage sequences
   function void make1(seq_item s);
     cseq1.push_back(s);
   endfunction
@@ -26,59 +21,89 @@ class seq_covercross extends uvm_sequence #(seq_item);
     cseq2.push_back(s);
   endfunction
 
-    // add items to queue to define coverage sequence pattern
   function void display();
     for (int i=0; i<cseq1.size(); i++)
       `uvm_info("cseq1",$sformatf("%s sequence coverage pattern rd_wr==%0d slv_addr=%0d",st,cseq1[i].rd_wr, cseq1[i].slv_addr),UVM_LOW)
-      for (int i=0; i<cseq2.size(); i++)
-        `uvm_info("cseq2",$sformatf("%s sequence coverage pattern rd_wr==%0d slv_addr=%0d",st,cseq2[i].rd_wr, cseq2[i].slv_addr),UVM_LOW)
+    for (int i=0; i<cseq2.size(); i++)
+      `uvm_info("cseq2",$sformatf("%s sequence coverage pattern rd_wr==%0d slv_addr=%0d",st,cseq2[i].rd_wr, cseq2[i].slv_addr),UVM_LOW)
   endfunction
 
   // update coverage using sample transaction 
   function void update(seq_item s);
     q.push_back(s);
     if (q.size() == cseq1.size() + cseq2.size()) begin
-      if (check()) begin
+      if (docoverage()) begin
         hit = hit + 1;
-        `uvm_info("cov_sequence",$sformatf("%s sequence coverage cross hit =%0d",st,hit),UVM_LOW)       
+        //`uvm_info("cov_sequence",$sformatf("%s sequence coverage cross hit =%0d",st,hit),UVM_LOW)       
       end
       q.pop_front();
     end
   endfunction
   
-  //checks if coverage is deteccted cseq2 within cseq1
-  //cseq2 can be non-consecutive within
-  function logic check();
-    int index1, index2; 
+  //checks if coverage is hit
+  //sampled transactions can be non-consecutive within cseq
+  // 4 modes: either-sided within, one-sided within, overlapped, non-overlapped
+  function logic docoverage(int mode = 2);
+    int j, start1, end1, start2, end2;
     
-    index1 = 1; index2 = 0;
-    //start and end match cseq1 start and end (within)
-    if (!((match(cseq1, 0, q[0]) && match(cseq1, cseq1.size()-1, q[$])))) return 0; 
-    //inside matches either
-    for (int i=1; i<q.size()-1; i++) 
-      if (match(cseq1, index1, q[i])) begin
-        index1 = index1 + 1;
-        if (index1 > cseq1.size()-1) return 0;
+    //check if cseq1 & cseq2 are hit inside q
+    j=0; start1 = 0; end1 = 0;
+    for (int i=0; i<q.size() && j<cseq1.size(); i++)
+      if (match(q, i, cseq1[j])) begin
+        if (j==0) start1 = i;
+        if (j==cseq1.size()-1) end1 = i;
+        j=j+1;
       end
-      else if (match(cseq2, index2, q[i])) begin
-        index2 = index2 + 1;
-        if (index2 > cseq2.size()) return 0;
+    if (j != cseq1.size()) return 0;
+    //`uvm_info("cov_sequence",$sformatf("single-interface time1 start1=%t start2=%t end1=%t end2=%t",start1, start2, end1, end2),UVM_LOW);
+    
+    j=0; start2 = 0; end2 = 0;
+    for (int i=0; i<q.size() && j<cseq2.size(); i++)
+      if (match(q, i, cseq2[j])) begin
+        if (j==0) start2 = i;
+        if (j==(cseq2.size()-1)) end2 = i;
+        j=j+1;
       end
-      else return 0;
-    for (int i = 0; i<q.size(); i++)
-      `uvm_info("seq_covercross",$sformatf("sequence coverage cross hit q[i].rd_wr=%0d q[i].slv_addr=%0d",q[i].rd_wr, q[i].slv_addr),UVM_LOW)
-    return 1;
+    if (j != cseq2.size()) return 0;
+    //`uvm_info("cov_sequence",$sformatf("single-interface time2 start1=%t start2=%t end1=%t end2=%t",start1, start2, end1, end2),UVM_LOW);
+            
+    	//seq2 within seq1 only (one-sided)
+    if (((mode==1) && ((start1 < start2) && (end1 > end2))) || 
+        //both seq2 within seq1 and
+        ((mode == 0) && (((start1 < start2) && (end1 > end2)) || 
+                         //seq1 within seq2 (two-sided)
+                         ((start2 < start1) && (end2 > end1)))) || 
+        //overlapped seq1 first 
+        ((mode == 2) && (((start1 < start2) && (end1 > start2) && (end2 > end1)) || 
+                         //overlapped seq2 first 
+                         ((start2 < start1) && (end2 > start1) &&(end1 > end2)) || 
+                         //overlapped seq2 within seq1
+                         ((start1 < start2) && (end1 > end2)) || 
+                         //overlapped seq1 within seq2
+                         ((start2 < start1) && (end2 > end1)))) || 
+        //non-overlapped seq1 first 
+        ((mode == 3) && ((end1 < start2) || 
+                         //non-overlapped seq2 first  
+                         (end2 < start1)))) 
+      begin
+        `uvm_info("cov_sequence",$sformatf("interface0 sequence coverage cross HIT"),UVM_LOW)    
+        `uvm_info("cov_sequence",$sformatf("mode=%d start1=%t start2=%t end1=%t end2=%t",mode, start1, start2, end1, end2),UVM_LOW);
+      	//for (int i = 0; i<q.size(); i++)
+        	//`uvm_info("seq_covercross",$sformatf("single-interface sequence coverage cross HIT q[i].rd_wr=%0d q[i].slv_addr=%0d",q[i].rd_wr, q[i].slv_addr),UVM_LOW)
+      	return 1;
+    	end
+    `uvm_info("cov_sequence",$sformatf("missed single-interface time start1=%t start2=%t end1=%t end2=%t",start1, start2, end1, end2),UVM_LOW);
+    return 0;
   endfunction
             
-  function void report();
-    `uvm_info("seq_covercross",$sformatf("%s sequence coverage crosshit =%0d",st,hit),UVM_LOW)
+  function void cseq_report();
+    `uvm_info("seq_covercross",$sformatf("%s sequence coverage cross hit =%0d",st,hit),UVM_LOW)
   endfunction
   
-  //either cseq1 or cseq2 match otherwise reset seq coverage
   function logic match(seq_item seq[$], int index, seq_item s);
     if (index == seq.size()) return 0;
     if ((s.rd_wr == seq[index].rd_wr) && (s.slv_addr == seq[index].slv_addr)) begin
-      `uvm_info("seq_covercross cseq1",$sformatf("%s transaction coverage match s.rd_wr==%0d s.slv_addr=%0d seq.rd_wr==%0d seq.slv_addr=%0d",st,s.rd_wr, s.slv_addr, seq[index].rd_wr, seq[index].slv_addr),UVM_LOW)    
+      //`uvm_info("seq_covercross cseq1",$sformatf("%s transaction coverage match s.rd_wr==%0d s.slv_addr=%0d seq.rd_wr==%0d seq.slv_addr=%0d",st,s.rd_wr, s.slv_addr, seq[index].rd_wr, seq[index].slv_addr),UVM_LOW)    
       return 1;
     end
     return 0;
